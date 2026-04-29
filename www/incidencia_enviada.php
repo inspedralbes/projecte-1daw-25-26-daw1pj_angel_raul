f8d96045087c1019d5222223a20bc7aa2967957d
a25raudialom <a25raudialom@inspedralbes.cat>
2026-04-29 10:46:40 +0200

    Creacion de incidencia_enviada utilizando boostrap,  todo lo de dentro de crear una incidencia está creado (el esqueleto)

---
+++ www/incidencia_enviada.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Incidencia enviada</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<header class="border-bottom py-2 px-3 d-flex justify-content-between align-items-center bg-white shadow-sm">
+    <div class="d-flex align-items-center">
+        <img src="IMG/LogoEmpresa.jpg" alt="Logo" style="height: 40px;">
+    </div>
+
+    <a href="index.php" class="btn btn-outline-primary d-flex align-items-center">
+        <i class="bi bi-house-door-fill" style="font-size: 1.3rem;"></i>
+    </a>
+</header>
+
+<h1 class="text-center fw-bold my-4">INCIDENCIA ENVIADA</h1>
+
+<body class="bg-light">
+
+    <div class="container">
+<div class="card p-4 shadow-sm mx-auto d-flex justify-content-center text-center" 
+     style="max-width: 800px; min-height: 400px;">
+
+    <h3 class="fw-bold mb-4" style="font-size: 2rem;">¡Tu incidencia ha sido registrada!</h3>
+
+    <p class="mb-4" style="font-size: 1.3rem;">
+        Tu número de incidencia es:
+    </p>
+
+    <span class="fw-bold text-primary" style="font-size: 2.5rem;">
+        00000
+    </span>
+
+</div>
+
+            </p>
+            </a>
+
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
